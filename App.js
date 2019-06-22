3cfb6d3ebf532a6b8a45535902413faf0f8c4d94
Sibcgh <16200278+Sibcgh@users.noreply.github.com>
2019-06-22 01:20:18 -0400

    useless

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class MyComponent extends Component {
+  
+  constructor() {
+  
+    super();
+
+    this.state = {
+      text: ''
+    }
+  
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container }>
+
+        
+        <Text>
+          test post pls ignore
+        </Text>
+        < TextInput style={styles.text}
+          label='Email'
+          value={this.state.text}
+          onChangeText={text => this.setState({ text })}
+        />
+      </View>
+
+    );
+
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
+    height: '100%',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  text: {
+    marginTop: 10,
+    width: '100%'
+  }
 });
