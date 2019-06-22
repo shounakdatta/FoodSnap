14e876ea2d45b9f05edb34468c6e2c6db311d142
Sibcgh <16200278+Sibcgh@users.noreply.github.com>
2019-06-22 14:42:04 -0400

    a lot of changes with adding some inital apps

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { Button, TextInput } from 'react-native-paper';
+
 
 
 export default class MyComponent extends Component {
@@ -10,7 +11,9 @@ export default class MyComponent extends Component {
     super();
 
     this.state = {
-      text: ''
+      email: null,
+      password: null,
+      severError: false
     }
   
   }
@@ -20,24 +23,63 @@ export default class MyComponent extends Component {
     return (
       <View style={styles.container }>
 
-        
-        <Text>
-          test post pls ignore
-        </Text>
+        <Text style={{ fontSize: 30 }}>FoodSnap</Text>
+
         < TextInput style={styles.text}
           label='Email'
-          value={this.state.text}
-          onChangeText={text => this.setState({ text })}
+          value={this.state.email}
+          autoFocus
+
+          onChangeText={text => this.setState({ email : text})}
         />
+        < TextInput style={styles.text}
+          label='Password'
+          value={this.state.password}
+          onChangeText={text => this.setState({ password : text })}
+        />
+        {
+          this.state.severError ?
+          <Text style={{color: 'red',textAlign: 'center'}}>
+            Error Login
+          </Text> :
+          null  
+        }
+
+
+        <Button style={{ marginTop: 20, height: '5%', width: '100%'}} icon="add-a-photo" mode="contained" onPress={() => console.log('Pressed')}>
+          Log in!
+        </Button>       
+
       </View>
 
-    );
+    )
+  }
 
-  };
+userTyping = (whichInput, event) => {
+  switch(whichInput){
+    case 'email':
+      this.setState({email: event.target.value});
+      break;
+  case 'password':
+    this.setState({ password: event.target.value });
+    break;    
+  default:
+    break;
+
+  }
 }
+
+
+
+
+
+}
+
+ 
 
 const styles = StyleSheet.create({
   container: {
+    padding: 50,
     height: '100%',
     flex: 1,
     backgroundColor: '#fff',
